72914692d2f37ae4b61022779d8b5a957de824ad
gokutheengineer <engineerzubizaretta@gmail.com>
2023-09-20 14:37:16 +0300

    add alias

--- main.tf
+++ main.tf
@@ -1,7 +1,36 @@
 provider "aws" {
-    region = "us-east-2"
+    region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "first_bucket" {
-    bucket = "gokhan-first-bucket-terraform"
+provider "aws" {
+    alias = "ohio"
+    region = "us-east-2"
+} 
+
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "my_security_group" {
+    name = "Example Security Group"
+    description = "Allow HTTP and SSH inbound traffic"
+    vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_security_group_rule" "tls_in" {
+    protocol = "tcp"
+    security_group_id = aws_security_group.my_security_group.id
+    from_port = 443
+    to_port = 443
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "http_in"{
+    protocol = "tcp"
+    security_group_id = aws_security_group.my_security_group.id
+    from_port = 80
+    to_port = 80
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"]
 }
